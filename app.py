c2ac96d7de149b2cd16e6a4048da954872b741c0
Ethan Reynolds <ethan.t.reynolds@gmail.com>
2023-01-18 15:44:28 -0700

    update app and models

--- app.py
+++ app.py
@@ -1,12 +1,23 @@
 """Blogly application."""
 
-from flask import Flask
-from models import db, connect_db
+from flask import Flask, request, redirect, render_template
+from flask_debugtoolbar import DebugToolbarExtension
+from models import db, connect_db, User
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql:///blogly'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.config['SQLALCHEMY_ECHO'] = True
+app.config['SECRET_KEY'] = 'keepitsecret'
+
+toolbar = DebugToolbarExtension(app)
 
 connect_db(app)
-db.create_all()
+# db.create_all()
+
+
+@app.route('/')
+def home_page():
+    """Home page."""
+
+    return render_template('index.html')
